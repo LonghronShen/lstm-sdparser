cc529c1c4eda5c523e902bf327413216404d2aba
Steven Hangger <longhronshen@hotmail.com>
2020-12-19 10:23:47 +0800

    update

--- utilities/build.sh
+++ utilities/build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 export WITH_CUDA=${1:-off}
+export CUDA_ARCH=${2:-500}
 
 mkdir -p build
 
 cd build
-cmake -DWITH_CUDA="${WITH_CUDA}" ..
+cmake -DWITH_CUDA="${WITH_CUDA}" -DCUDA_ARCH="${CUDA_ARCH}" ..
 
 cmake --build . -j$(nproc)
